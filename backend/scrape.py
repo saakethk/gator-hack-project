9a8354ef9de69e990de8ed1734fcefc7f4103b30
Saaketh Kesireddy <saakethr.kesireddy@gmail.com>
2025-10-25 15:06:12 -0400

    Merged backend together

---
+++ backend/scrape.py
@@ -0,0 +1,111 @@
+
+from topic import Topic, get_utc_timestamp, parse_date
+import requests
+from bs4 import BeautifulSoup
+import time
+import json
+from general import NAVIGATOR_CLIENT, REDDIT_CLIENT
+
+# For performance analysis
+def timer(func):
+    """
+    A decorator that measures the execution time of a function.
+    """
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()  # Record start time
+        result = func(*args, **kwargs)    # Execute the wrapped function
+        end_time = time.perf_counter()    # Record end time
+        execution_time = end_time - start_time
+        print(f"Function '{func.__name__}' executed in {execution_time:.4f} seconds.")
+        return result
+    return wrapper
+    
+# Ask llm to identify whether 
+@timer
+def filter_post(topic: str) -> tuple[bool, str]:
+    response = NAVIGATOR_CLIENT.complete(
+        prompt=f"The title of a post is {topic}. Your role is to identify whether this title contains a software technology or is irrelevant. " \
+        "Return a json response with the key 'relevant' which has true or false and extract just the name of the technology under the key 'name'. Response should only contain JSON and look like:" \
+        "{'relevant': false, 'name': 'python'}")
+    try:
+        response_parsed = json.loads(str(response))
+        if response_parsed["name"] != "" and response_parsed["name"] != None:
+            return response_parsed["relevant"], response_parsed["name"]
+        else:
+            return False, response_parsed["name"]
+    except Exception as error:
+        return False, str(error)
+    
+# Parse the url to make 
+@timer
+def prompt_about_url(prompt: str, url: str) -> str:
+    context_response = requests.get(url)
+    soup = BeautifulSoup(context_response.text, 'html.parser')
+    condensed_content = " ".join(soup.text.split())
+    response = NAVIGATOR_CLIENT.complete(
+        prompt=f"Website context: {condensed_content}. Utilize the website content above for the provided prompt. Prompt: {prompt}")
+    return str(response)
+
+# Parse the actual topic based on website context
+@timer
+def parse_website_topic(url: str, name: str):
+    summary_response = prompt_about_url(
+        prompt="Summarize the following website into a short 100-150 word summary, provide the name of the tool, and provide a list of pros and cons. " \
+        "Response should only contain JSON and look like:" \
+        "{\"name\": \"name goes here\", \"summary\": \"summary goes here\", \"pros\": [\"pros goes here\"], \"cons\": [\"cons goes here\"]}", 
+        url=url)
+    try:
+        summary_parsed = json.loads(summary_response)
+        return True, summary_parsed["name"], summary_parsed["summary"], summary_parsed["pros"], summary_parsed["cons"]
+    except Exception as error:
+        return False, error, error, error, error
+
+# Gets reddit posts
+@timer
+def get_reddit_posts(subreddit_name: str, limit: int, exceptions: list[str]) -> list[Topic]:
+    # Gets subreddit
+    subreddit = REDDIT_CLIENT.subreddit(subreddit_name)
+    # Fetch posts
+    topics = []
+    for submission in subreddit.rising(limit=limit):
+        valid, parsed_name = filter_post(topic=submission.title)
+        if valid:
+            summary_status, filtered_name, summary, pros, cons = parse_website_topic(url=submission.url, name=parsed_name)
+            if summary_status:
+                valid_name = True # Check that it is not in exceptions
+                for exception in exceptions:
+                    if exception.lower() in filtered_name.lower(): # type: ignore
+                        valid_name = False
+                if valid_name:
+                    topic = Topic(
+                        name=filtered_name, # type: ignore
+                        url=submission.url,
+                        summary=summary, # type: ignore
+                        date_added=get_utc_timestamp(),
+                        date_created=parse_date(submission.created_utc),
+                        source="reddit",
+                        is_archived=False, # Should always be false when first generated
+                        is_active=True,
+                        internal_relevance_score=0,
+                        relevance_score=0,
+                        exercises=[],
+                        pros=pros, # type: ignore
+                        cons=cons) # type: ignore
+                    topics.append(topic)
+    return topics
+
+# Gets all filtered topics
+@timer
+def get_all_topics(source_limit: int = 10, exceptions: list[str] = ["reddit", 'github']) -> list[Topic]:
+    relevant_subreddits = ["programming", "coding", "learnprogramming", "creativecoding"]
+    all_topics = []
+    for subreddit in relevant_subreddits:
+        for item in get_reddit_posts(subreddit_name=subreddit, limit=source_limit, exceptions=exceptions):
+            if item not in all_topics:
+                all_topics.append(item)
+    return all_topics
+
+def test_scrape():
+    topics = get_all_topics(source_limit=5)
+    for topic in topics:
+        print(topic.get_dict())
